1e2f0c11141d721d2a334ffebc8387e43591bcaa
dario-ramos <dario.fenix@gmail.com>
2018-04-27 17:44:48 -0300

    [RA-48] Forgot file

---
+++ bitprim.insight/Constants.cs
@@ -0,0 +1,9 @@
+namespace bitprim.insight
+{
+    public class Constants
+    {
+        public const string LONG_CACHE_PROFILE_NAME = "Long";
+        public const string SHORT_CACHE_PROFILE_NAME = "Short";
+    }
+
+}
