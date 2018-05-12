0d3c4c8841c0787f902be2959c7b4de7dd7d3d99
Mario Dal Lago <the4power@yahoo.com.ar>
2018-05-12 20:26:53 -0300

    add method signature

---
+++ bitprim.insight/Controllers/MessageController.cs
@@ -0,0 +1,21 @@
+using System.Dynamic;
+using Microsoft.AspNetCore.Mvc;
+
+namespace bitprim.insight.Controllers
+{
+    [Route("[controller]")]
+    public class MessageController : Controller
+    {
+        [HttpGet("messages/verify")]
+        [HttpPost("messages/verify")]
+        public ActionResult Verify(string address, string signature, string message)
+        {
+            //Dummy return
+            return StatusCode((int)System.Net.HttpStatusCode.BadRequest, "Unexpected error:");
+            
+            //TODO fully implementation
+            dynamic result = new ExpandoObject();
+            return Json(result);  
+        }
+    }
+}
