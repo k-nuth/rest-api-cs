45c0815a519c09eef493665fad2364e87c487fa2
Mario <the4power@yahoo.com.ar>
2018-09-11 10:52:57 -0300

    reorder fields

--- bitprim.insight/DTOs/GetBlocksByDateResponse.cs
+++ bitprim.insight/DTOs/GetBlocksByDateResponse.cs
@@ -29,9 +29,14 @@ namespace bitprim.insight.DTOs
     public class BlockSummary
     {
         /// <summary>
-        /// Block miner pool info (if applies).
+        /// Block height.
         /// </summary>
-        public PoolInfo poolInfo { get; set; }
+        public UInt64 height { get; set; }
+
+        /// <summary>
+        /// Serialized block size.
+        /// </summary>
+        public UInt64 size { get; set; }
 
         /// <summary>
         /// Block hash as 64-character (32 bytes) hex string.
@@ -44,19 +49,16 @@ namespace bitprim.insight.DTOs
         public UInt32 time { get; set; }
 
         /// <summary>
-        /// Block height.
-        /// </summary>
-        public UInt64 height { get; set; }
-
-        /// <summary>
-        /// Serialized block size.
-        /// </summary>
-        public UInt64 size { get; set; }
-
-        /// <summary>
         /// Amount of transactions in the block.
         /// </summary>
         public UInt64 txlength { get; set; }
+
+        /// <summary>
+        /// Block miner pool info (if applies).
+        /// </summary>
+        public PoolInfo poolInfo { get; set; }
+
+      
     }
 
     /// <summary>
@@ -64,6 +66,26 @@ namespace bitprim.insight.DTOs
     /// </summary>
     public class Pagination
     {
+        /// <summary>
+        /// Day after search date.
+        /// </summary>
+        public string next { get; set; }
+
+        /// <summary>
+        /// Day before search date.
+        /// </summary>
+        public string prev { get; set; }
+
+        /// <summary>
+        /// Unix timestamp; marks the end of the search interval.
+        /// </summary>
+        public long currentTs { get; set; }
+
+        /// <summary>
+        /// Search date in configured date input format (default: yyyy-MM-dd).
+        /// </summary>
+        public string current { get; set; }
+
         /// <summary>
         /// True if and only if the block date to search corresponds to today.
         /// </summary>
@@ -75,28 +97,9 @@ namespace bitprim.insight.DTOs
         public bool more { get; set; }
 
         /// <summary>
-        /// Unix timestamp; marks the end of the search interval.
-        /// </summary>
-        public long currentTs { get; set; }
-
-        /// <summary>
         /// Unix timestamp of the first block beyond (i.e. before in the blockchain) the selected interval.
         /// </summary>
-        public long? moreTs { get; set; }
-
-        /// <summary>
-        /// Search date in configured date input format (default: yyyy-MM-dd).
-        /// </summary>
-        public string current { get; set; }
-
-        /// <summary>
-        /// Day after search date.
-        /// </summary>
-        public string next { get; set; }
-
-        /// <summary>
-        /// Day before search date.
-        /// </summary>
-        public string prev { get; set; }
+        public long? moreTs { get; set; }        
+        
     }
 }
