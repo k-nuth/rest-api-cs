b82db14f7328e6176593ea5a1494cb29e2a31cb8
Dario Ramos <dario.fenix@gmail.com>
2018-09-28 10:52:36 -0300

    [RA-236] Do not write vin properties if null

--- bitprim.insight/DTOs/TransactionSummary.cs
+++ bitprim.insight/DTOs/TransactionSummary.cs
@@ -1,4 +1,5 @@
 using System;
+using Newtonsoft.Json;
 
 namespace bitprim.insight.DTOs
 {
@@ -93,13 +94,21 @@ namespace bitprim.insight.DTOs
     public class TransactionInputSummary
     {
         /// <summary>
+        /// For a coinbase transaction, hex representation of the coinbase script.
+        /// For a non-coinbase transaction, this field will be empty.
+        /// </summary>
+        public string coinbase { get; set; }
+
+        /// <summary>
         /// References the input's previous output.
         /// </summary>
+        [JsonProperty(NullValueHandling=NullValueHandling.Ignore)]
         public string txid { get; set; }
 
         /// <summary>
         /// References the input's previous output.
         /// </summary>
+        [JsonProperty(DefaultValueHandling = DefaultValueHandling.Ignore)]
         public UInt32 vout { get; set; }
 
         /// <summary>
@@ -115,37 +124,35 @@ namespace bitprim.insight.DTOs
         /// <summary>
         /// Input script.
         /// </summary>
+        [JsonProperty(NullValueHandling=NullValueHandling.Ignore)]
         public InputScriptSummary scriptSig { get; set; }
 
         
         /// <summary>
         /// Previous output destination address.
         /// </summary>
+        [JsonProperty(NullValueHandling=NullValueHandling.Ignore)]
         public string addr { get; set; }
 
         
         /// <summary>
         /// Output value in Satoshis.
         /// </summary>
+        [JsonProperty(DefaultValueHandling = DefaultValueHandling.Ignore)]
         public UInt64 valueSat { get; set; }
         
         /// <summary>
         /// Input value in coin units.
         /// </summary>
+        [JsonProperty(DefaultValueHandling = DefaultValueHandling.Ignore)]
         public decimal value { get; set; }
 
         /// <summary>
         /// If this input is a double spend, this will point to the conflicting transaction.
         /// </summary>
+        [JsonProperty(NullValueHandling=NullValueHandling.Ignore)]
         public string doubleSpentTxID { get; set; }
 
-        
-        /// <summary>
-        /// For a coinbase transaction, hex representation of the coinbase script.
-        /// For a non-coinbase transaction, this field will be empty.
-        /// </summary>
-        public string coinbase { get; set; }
-        
     }
 
     /// <summary>
